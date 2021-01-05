768af2ac2386379100b205c9a650eca08a9062e6
sivasriram <connectsivaram66@gmail.com>
2021-01-05 02:55:43 +0000

    this is the commit

--- demo1.java
+++ demo1.java
@@ -2,9 +2,9 @@ class demo1
 {
 public static void  main (string args [])
 { 
-	system.out.println("java demo class 1");
+	system.out.println("java demo class 1 ");
 
-system.out.println("thsia ia second line xava demo class 1")}
+}
 
 
 }
